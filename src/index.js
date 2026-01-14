b32edb69bd31ed4d4b543f44c1824563c3a7fc99
Busiswa Bili-bili <141039125+BUSISWABILIBILI@users.noreply.github.com>
2026-01-14 11:03:44 +0200

    Add weather elements

--- src/index.js
+++ src/index.js
@@ -3,11 +3,39 @@ function refreshWeatherData(response) {
   let humidityElement = document.querySelector("#humidity-value");
   let windElement = document.querySelector("#wind-value");
   let cityElement = document.querySelector("#city-name");
+  let descriptionElement = document.querySelector("#description");
+  let timeElement = document.querySelector("#weather-time");
+  let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#temperature");
 
   cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
+  descriptionElement.innerHTML = response.data.condition.description;
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
-  humidityElement.innerHTML = response.data.temperature.humidity;
-  windElement.innerHTML = Math.round(response.data.wind.speed);
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windElement.innerHTML = `${response.data.wind.speed} km/h`;
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="temperature-icon-img" />`;
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day}, ${hours}:${minutes} ${hours >= 12 ? "PM" : "AM"}`;
 }
 
 function searchCity(city) {
