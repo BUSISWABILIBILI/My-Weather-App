61155771d2d18e99d406abc99a654f15c92214b4
Busiswa Bili-bili <141039125+BUSISWABILIBILI@users.noreply.github.com>
2026-01-14 09:59:57 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
+function refreshWeatherData(response) {
+  let temperatureElement = document.querySelector("#temperature-value");
+  let humidityElement = document.querySelector("#humidity-value");
+  let windElement = document.querySelector("#wind-value");
+  let cityElement = document.querySelector("#city-name");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+function searchCity(city) {
+  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeatherData);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city-name");
-  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Paris");
