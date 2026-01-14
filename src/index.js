81a3727dfe935c1ff82fa2e0c162ee4b6428148c
Busiswa Bili-bili <141039125+BUSISWABILIBILI@users.noreply.github.com>
2026-01-14 16:42:19 +0200

    Replace demo content with real api content to display the real content

--- src/index.js
+++ src/index.js
@@ -45,22 +45,35 @@ function searchCity(city) {
   axios.get(apiUrl).then(refreshWeatherData);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[date.getDay()];
+}
+
 function displayForecast(response) {
-  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
   let forecastHtml = "";
 
-  days.forEach(function (day) {
-    forecastHtml += `
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml += `
     <div class="weather-forecast-day">
-      <div class="weather-forecast-date">${day}</div>
-      <div class="weather-forecast-icon">☀️</div>
+      <div class="weather-forecast-date">${formatDay(day.time)}</div>
+      <div>
+        <img src="${
+          day.condition.icon_url
+        }" class="weather-forecast-icon" alt="Weather Icon" />
+      </div>
       <div class="weather-forecast-temperatures">
         <div class="weather-forecast-temperature">
-          <strong>15°C</strong>
+          <strong>${Math.round(day.temperature.maximum)}°</strong>
         </div>
-        <div class="weather-forecast-temperature">9°C</div>
+        <div class="weather-forecast-temperature">${Math.round(
+          day.temperature.minimum
+        )}°</div>
       </div>
     </div>`;
+    }
   });
 
   let forecastElement = document.querySelector("#forecast");
