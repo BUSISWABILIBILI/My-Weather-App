e7832ddf229f175f59d9f0d57f10760539fd9655
Busiswa Bili-bili <141039125+BUSISWABILIBILI@users.noreply.github.com>
2026-01-14 13:40:14 +0200

    Inject the HTML forecast from JavaScript

--- src/index.js
+++ src/index.js
@@ -44,6 +44,28 @@ function searchCity(city) {
   axios.get(apiUrl).then(refreshWeatherData);
 }
 
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml += `
+    <div class="weather-forecast-day">
+      <div class="weather-forecast-date">${day}</div>
+      <div class="weather-forecast-icon">☀️</div>
+      <div class="weather-forecast-temperatures">
+        <div class="weather-forecast-temperature">
+          <strong>15°C</strong>
+        </div>
+        <div class="weather-forecast-temperature">9°C</div>
+      </div>
+    </div>`;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
@@ -53,3 +75,4 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Paris");
+displayForecast();
